d1a1cbc336ac48c4ac8e35418d8c47e7531587bc
Tino Ganacias <84544540+tinoganacias@users.noreply.github.com>
2021-09-06 00:58:48 -0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const compression = require("compression");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(compression());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+// routes
+app.use(require("./routes/api.js"));
+
+app.listen(PORT, () => {
+  console.log(`listening to the smoov server sounds on PORT ${PORT}!`);
+});
